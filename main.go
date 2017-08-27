e14effbf885b375189014b964cc3bc3cbfd2da4f
David Gage <whytheplatypus@gmail.com>
2017-08-27 18:18:40 -0400

    Interfaces for rows and colors

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"image/color"
+	"io"
 	"time"
+
+	"golang.org/x/image/colornames"
 
 	"github.com/godbus/dbus"
 )
@@ -24,37 +28,127 @@ func main() {
 	}
 	for _, v := range list {
 		fmt.Println(v)
-		var d float64
-		if err := conn.Object("org.razer", dbus.ObjectPath("/org/razer/device/"+v)).Call("razer.device.lighting.brightness.getBrightness", 0).Store(&d); err != nil {
+		var m []int32
+		if err := conn.Object("org.razer", dbus.ObjectPath("/org/razer/device/"+v)).Call("razer.device.misc.getMatrixDimensions", 0).Store(&m); err != nil {
 			panic(err)
 		}
-		fmt.Println(d)
+		fmt.Println(m)
+		reset := &Set{
+			rows: make([]*Row, int(m[0])),
+		}
+		for i, _ := range reset.rows {
+			reset.rows[i] = &Row{
+				num:    uint8(i),
+				start:  0,
+				colors: make([]Color, int(m[1])),
+			}
+			for k, _ := range reset.rows[i].colors {
+				reset.rows[i].colors[k] = &RGBA{colornames.Purple}
+			}
+		}
+		Apply(v, conn, reset)
+		/*
+			Apply(v, conn, &Set{
+				[]*Row{
+					&Row{
+						num: 3,
+						colors: []Color{
+							&RGB{
+								10, 0, 0,
+							},
+							&RGB{
+								10, 0, 10,
+							},
+							&RGB{
+								0, 0, 100,
+							},
+							//&RGBA{colornames.Purple},
+						},
+					},
+				},
+			})
+			/**/
 
-		SetRow(v, conn)
 		SetCustom(v, conn)
 	}
 }
 
-func SetRow(name string, conn *dbus.Conn) {
-	test := []uint8{10, 0, 0, 0, 10, 0, 0, 0, 10, 10, 0, 0}
+type RGBA struct {
+	color.RGBA
+}
+
+func (rgba *RGBA) RGB() []uint8 {
+	return []uint8{rgba.R, rgba.G, rgba.B}
+}
+
+type RGB struct {
+	r, g, b uint8
+}
+
+func (rgb *RGB) RGB() []uint8 {
+	return []uint8{rgb.r, rgb.g, rgb.b}
+}
+
+type Color interface {
+	RGB() []uint8
+}
+
+// TODO rename
+type Row struct {
+	num uint8
+	// can make this matter but requires updates to the driver I think?
+	start  uint8
+	colors []Color
+}
+
+func (r *Row) Encode(w io.Writer) error {
+	if err := binary.Write(w, binary.LittleEndian, r.num); err != nil {
+		return err
+	}
+	if err := binary.Write(w, binary.LittleEndian, r.start); err != nil {
+		return err
+	}
+	/**/
+	offset := int(r.start)*2 + len(r.colors) - 1
+	fmt.Println(offset, int(r.start))
+	/**/
+	stopcol := offset
+	if err := binary.Write(w, binary.LittleEndian, uint8(stopcol)); err != nil {
+		return err
+	}
+	for _, c := range r.colors {
+		if err := binary.Write(w, binary.LittleEndian, append([]uint8{}, c.RGB()...)); err != nil {
+			return err
+		}
+	}
+	/**/
+	if err := binary.Write(w, binary.LittleEndian, make([]uint8, int(r.start)*3)); err != nil {
+		return err
+	}
+	/**/
+
+	return nil
+}
+
+// TODO rename
+type Set struct {
+	rows []*Row
+}
+
+func (s *Set) Encode(w io.Writer) error {
+	for _, r := range s.rows {
+		if err := r.Encode(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Apply(name string, conn *dbus.Conn, s *Set) {
+
 	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, []uint8{0})
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	err = binary.Write(buf, binary.BigEndian, []uint8{0})
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	err = binary.Write(buf, binary.BigEndian, uint8(len(test)/3-1))
-	//err = binary.Write(buf, binary.LittleEndian, []int16{})
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	err = binary.Write(buf, binary.LittleEndian, test)
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
+	s.Encode(buf)
+
 	fmt.Printf("% x", buf.Bytes())
 	// test := []byte{3, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 255, 00, 255, 00, 00}
 	c := conn.Object("org.razer", dbus.ObjectPath("/org/razer/device/"+name)).Call("razer.device.lighting.chroma.setKeyRow", 0, buf.Bytes())
